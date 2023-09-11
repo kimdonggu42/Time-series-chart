7a7f50147c898bc968a4182ec7c07b7ffa35d8f1
kimdonggu42 <kdonggoo44@gmail.com>
2023-09-11 16:57:24 +0900

    feat: mock data 재가공

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import mockData from "./mocks/data.json";
+
+interface MockData {
+  id: string;
+  value_area: number;
+  value_bar: number;
+}
+
 export default function App() {
+  const res = mockData.response as Record<string, MockData>;
+  const resData = Object.keys(res).map((key) => {
+    return { ...res[key], time: key.split(" ")[1] };
+  });
+
   return <></>;
 }
